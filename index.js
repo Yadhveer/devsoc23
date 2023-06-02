ae100a8454ba4fe496e5c4664da51f7f0440d8c7
Yadhveer R M <103442910+Yadhveer@users.noreply.github.com>
2023-06-03 00:14:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let no_of_signals = prompt("No. of traffic signals");
+let distance = [];
+let no_of_intersections = [];
+let red_time = [];
+
+let green_time = 60;
+let red_time[i] = (no_of_intersections[i]-1)*60;
+let realtime = 60;
+let time = 60;
+let max_speed = 70;
